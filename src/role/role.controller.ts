30e8808947a7a0e3e2ef1a1ba618da8103ddac70
bilal <mxbilal0@gmail.com>
2024-12-26 20:19:52 +0500

    removed token

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -59,10 +59,10 @@ export class RoleController {
         let res = await axios.get(
           `https://devapi-itsupport.lyca.sa/api/Role/GetRoleWithPermissionByIdAsync?Id=${i}`,
           {
-            headers: {
-              Authorization:
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzIiwianRpIjoiNDY5YmZmMzMtNGEzNC00YjU3LWIzNjAtMGRmYmM5MzE1NWYxIiwiaXNzIjoiTFlDQUFkbWluIiwiaWF0IjoxNzM1MTA0MjQ3LCJFbWFpbElkIjoiYWRtaW5AbHljYS5jb20uc2EiLCJOYW1lIjoiTXIgQWRtaW4iLCJUZW5hbnRJZCI6IjEiLCJVc2VySWQiOiIzIiwiUm9sZSI6IkFkbWluaXN0cmF0b3IiLCJKb2luaW5nRGF0ZSI6IjEyLzMvMjAyMyAxMjowMDowMCBBTSIsIm5iZiI6MTczNTEwNDI0NywiZXhwIjoxNzM1MTExNDQ3LCJhdWQiOiJMWUNBVXNlcnMifQ.hBoEvTAAn9y7rdv-n819_m3V9cn2KSFFrYlm8ztE36Q',
-            },
+            // headers: {
+            //   Authorization:
+            //     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzIiwianRpIjoiNDY5YmZmMzMtNGEzNC00YjU3LWIzNjAtMGRmYmM5MzE1NWYxIiwiaXNzIjoiTFlDQUFkbWluIiwiaWF0IjoxNzM1MTA0MjQ3LCJFbWFpbElkIjoiYWRtaW5AbHljYS5jb20uc2EiLCJOYW1lIjoiTXIgQWRtaW4iLCJUZW5hbnRJZCI6IjEiLCJVc2VySWQiOiIzIiwiUm9sZSI6IkFkbWluaXN0cmF0b3IiLCJKb2luaW5nRGF0ZSI6IjEyLzMvMjAyMyAxMjowMDowMCBBTSIsIm5iZiI6MTczNTEwNDI0NywiZXhwIjoxNzM1MTExNDQ3LCJhdWQiOiJMWUNBVXNlcnMifQ.hBoEvTAAn9y7rdv-n819_m3V9cn2KSFFrYlm8ztE36Q',
+            // },
           },
         );
         const { data } = res.data;
